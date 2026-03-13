c9528d71b54a8d8423e2fb03d829a65efd8c1937
Arshiyan-Elahi <arshiyanelahi84@gmail.com>
2026-03-13 14:46:30 +0500

    place holder Variable added and suggestions

--- src/components/contract/ReviewActions.jsx
+++ src/components/contract/ReviewActions.jsx
@@ -58,6 +58,7 @@ export default function ReviewActions({
                     {t.requestChanges}
                 </button>
 
+
                 <button
                     type="button"
                     onClick={setRejected}
